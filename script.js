720bd084bf1be1521dd4b0a24a41660f18e9518b
Naveenms48 <naveenms2411@gmail.com>
2025-02-10 17:50:11 +0530

    starts

--- script.js
+++ script.js
@@ -25,18 +25,33 @@ document.querySelectorAll('section').forEach(section => {
     });
 
     // When the mouse leaves the section
-    section.addEventListener('mouseleave', () => {
-      section.classList.remove('img-activeness');
-    });
 
     // When the section is focused (keyboard interaction)
     section.addEventListener('focusin', () => {
       section.classList.add('img-activeness');
     });
 
-    // When the section loses focus (keyboard interaction)
-    section.addEventListener('focusout', () => {
-      section.classList.remove('img-activeness');
+     // When the mouse enters the section
+     section.addEventListener('mouseenter', () => {
+      section.classList.add('content-activeness');
     });
-    
+
+    // When the mouse leaves the section
+
+    // When the section is focused (keyboard interaction)
+    section.addEventListener('focusin', () => {
+      section.classList.add('content-activeness');
+    });
+
+     // When the mouse enters the section
+     section.addEventListener('mouseleave', () => {
+      section.classList.remove('content-activeness');
+    });
+
+    // When the mouse leaves the section
+
+    // When the section is focused (keyboard interaction)
+    section.addEventListener('focusout', () => {
+      section.classList.remove('content-activeness');
+    });
   });
